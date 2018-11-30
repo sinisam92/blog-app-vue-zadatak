4509388834a62fbc00acdab7907b14cccfc4c84c
Sinisa Manojlovic <sinisamanojlovic1@gmail.com>
2018-11-30 19:00:09 +0100

    Zadatak 1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand">Posts exercise</a>
+            <router-link to="/posts">Posts</router-link>           
+        </nav>      
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import AppPosts from "./components/AppPosts.vue";
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', redirect: '/posts' },
+  { path: '/posts', component: AppPosts, name: 'posts'},
+
+];
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
