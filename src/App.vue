010d9a8ac85df2b3625fb83648bca80e0a9d753a
Sinisa Manojlovic <sinisamanojlovic1@gmail.com>
2018-12-03 15:10:49 +0100

    Particles.js

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,10 @@ export default {
     NavBar
   }
 };
+
+particlesJS.load("particles-js", "particles.json", function() {
+  console.log("callback - particles.js config loaded");
+});
 </script>
 
 <style>
@@ -23,5 +27,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  z-index: 100;
+  position: relative;
+}
+canvas {
+  width: 100%;
+  height: 100%;
+}
+#particles-js {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to right, rgb(58, 117, 156), rgb(0, 0, 0));
+  background-size: cover;
+}
+body {
+  height: 100%;
+  padding: 0px;
+  margin: 0px;
 }
 </style>
